093ce1b2fb5041146909918dcc077d4e68a577bf
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2021-07-27 20:12:04 -0300

    Nova mudança

--- Fila.hpp
+++ Fila.hpp
@@ -3,8 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#define MAX 100
 using namespace std;
 
+typedef struct TipoItem Item;
+typedef struct TipoFila Fila;
+
+struct TipoItem{
+	int dado;
+};
+struct TipoFila{
+	Item Fila[MAX];
+	int Frente;
+	int Fundo;
+};
 class Fila{
 	public:
 		
